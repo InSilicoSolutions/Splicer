8f0349dcfa63049e811d0203bdf01af52216c108
Michael Ryan <mryan@insilico.us.com>
2021-01-19 08:37:51 -0500

    Add

--- Splicer/splicerGraphBuild.py
+++ Splicer/splicerGraphBuild.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 import argparse
 
+#branch comment
 parser = argparse.ArgumentParser(description='Process initial database tables into refined SG tables.')
 parser.add_argument("Database", help='The path to where you want to store the database file')
 args = parser.parse_args()
